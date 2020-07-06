b54d5795141bd81779f0d7edec0355172e17f3d5
krzysztofwalczak <krzysztof.walczak1@unilodz.eu>
2020-07-06 03:55:06 +0200

    Git class changes

--- manifests/developer.pp
+++ manifests/developer.pp
@@ -1,13 +1,21 @@
 package { 'netbeans':
   ensure => installed,
 }
-
-class { 'java':
- package => 'openjdk-8-jre',
- java_alternative => 'java-1.8.0-openjdk-amd64',
- java_alternative_path => '/usr/lib/jvm/java-1.8.0-openjdk-amd64',
+package { 'docker.io':
+  ensure => installed,
 }
-
+class { 'java':
+  package => 'openjdk-8-jre',
+  java_alternative => 'java-1.8.0-openjdk-amd64',
+  java_alternative_path => '/usr/lib/jvm/java-1.8.0-openjdk-amd64',
+}
 class { 'git':
-
+}
+node 'dev01' {
+  git::config { 'user.name':
+    value => 'Krzysztof Walczak',
+  }
+  git::config { 'user.email':
+    value => 'krzysztof.walczak1@unilodz.eu'
+  }
 }
