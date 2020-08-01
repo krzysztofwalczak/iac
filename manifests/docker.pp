16dd6c2747969bcd2ba44efb4279877dbfb2e940
krzysztofwalczak <krzysztof.walczak1@unilodz.eu>
2020-08-01 16:02:33 +0200

    Loadbalancer environment

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -1,3 +1,13 @@
-class { 'docker': 
-    
+class { 'docker':
+
+}
+
+
+class {'docker::compose':
+  ensure => present,
+}
+
+docker_compose {'simple_environment':
+  compose_files => ['/opt/iac/environments/simple/docker-compose.yml'],
+  ensure => present,
 }
