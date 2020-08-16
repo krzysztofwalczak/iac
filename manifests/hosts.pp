b77a8daf00487eaee9312103cc5e3963ce90e907
krzysztofwalczak <krzysztof.walczak1@unilodz.eu>
2020-08-16 13:16:58 +0200

    Changes to hosts manifest

--- manifests/hosts.pp
+++ manifests/hosts.pp
@@ -1,21 +1,3 @@
-host { 'app01':
-    name => 'app01',
-    ensure => 'present',
-    ip => '172.17.0.3',
-}
-
-host { 'app02':
-    name => 'app02',
-    ensure => 'present',
-    ip => '172.17.0.4',
-}
-
-host { 'ihs01':
-    name => 'ihs01',
-    ensure => 'present',
-    ip => '172.17.0.2',
-}
-
 host { 'dev01':
     name => 'dev01',
     ensure => 'present',
